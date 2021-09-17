b872a98b96307fc7529d12df00fa8cdde3e74a2a
GitHub Action <action@github.com>
2021-09-17 15:59:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/psurkov/pynosetask/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/psurkov/pynosetask/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.psurkov.pynosetask.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.psurkov.pynosetask.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/psurkov/pynosetask/services/MyProjectService.kt
+++ src/main/kotlin/com/github/psurkov/pynosetask/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.psurkov.pynosetask.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.psurkov.pynosetask.MyBundle
 
 class MyProjectService(project: Project) {
 
